f5f7a11e672a198f416af2583f5523dbe60320f7
Matheus <matheuso.vidal@hotmail.com>
2020-02-24 14:15:52 -0300

    Modificações finais

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 from webscraping import *
 
 url_zip = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip"
+nome_zip = 'D_megase.zip'
+nome_html = 'd_mega.htm'
 
-baixar_arquivo(url_zip)
+web = Scraping(url_zip)
 
-mega_file = zipfile.ZipFile('D_megase.zip') #encontra o zip file
-mega_file.extract('d_mega.htm') #extrai somente o html necessário
-mega_file.close()
+web.extrairZip(nome_zip, nome_html)
 
-ref_arquivo = open("d_mega.htm","r") #lê esse html
+string = web.inserirHtmlVar(nome_html)
 
-string_arquivo = ref_arquivo.read() #insere o html numa variável
+conteudo = web.tratarHTML(string) 
 
-ref_arquivo.close()
-
-soup = BeautifulSoup(string_arquivo, 'html.parser') #faz o parsing do arquivo para html
-
-table = soup.find(name='table') #transforma a table em uma estrutura python
-
-dt_full = pd.read_html(str(table))[0] #lê o html e transforma novamente em string
-                                      #use .head(NUMERO_QUALQUER) para buscar somente os primeiros NUMERO_QUALQUER resultados
-
-print(dt_full)
 dicionario = {}
 
-dicionario['jogos'] = dt_full.to_dict('records')
+dicionario['jogos'] = conteudo.to_dict('records')
 
-json_file = json.dumps(dicionario)
-fp = open('mega_sena.json', 'w')
-fp.write(json_file)
-fp.close
+web.transformarDictToJSON(dicionario, 'mega_sena.json')
 
-os.remove('d_mega.htm')
-os.remove('D_megase.zip')
+arquivos = [nome_html, nome_zip]
+
+web.removerArquivos([nome_html, nome_zip])
+
 
 
 
--- webscraping.py
+++ webscraping.py
@@ -6,16 +6,59 @@ import json
 import os
 import zipfile
 
+class Scraping():
 
-def baixar_arquivo(url, endereco=None):
-    if endereco is None:
-        endereco = os.path.basename(url.split("?")[0])
-    
-    resposta = requests.get(url)
-    if resposta.status_code == requests.codes.OK:
-        with open(endereco, 'wb') as novo_arquivo:
-                novo_arquivo.write(resposta.content)
-        print("Download finalizado. Arquivo salvo em: {}".format(endereco))
-    else:
-        resposta.raise_for_status()
+    def __init__(self, url, endereco=None):
+        super().__init__()
 
+        if endereco is None:
+            endereco = os.path.basename(url.split("?")[0])
+
+        #Download do arquivo, o escrevendo em disco e substituindo caso exista.
+        resposta = requests.get(url)
+        if resposta.status_code == requests.codes.OK:
+            with open(endereco, 'wb') as novo_arquivo:
+                    novo_arquivo.write(resposta.content)
+            print("Download finalizado!")
+        else:
+            print("Erro no download")
+            resposta.raise_for_status()
+
+    def extrairZip(self, nome_zip, nome_html):
+
+        # Busca o arquivo zip, extrai o HTML necessário e fecha o arquivo.
+        arquivo = zipfile.ZipFile(nome_zip)
+        arquivo.extract(nome_html)
+        arquivo.close()
+
+    def inserirHtmlVar(self, nome_html):
+        
+        #Lê o HTML, insere em uma variável e a retorna
+        ref_arquivo = open(nome_html,"r")
+        string_arquivo = ref_arquivo.read()
+        ref_arquivo.close()
+
+        return string_arquivo
+
+    def tratarHTML(self, string):
+        
+        #Faz o parsing do arquivo para HTML e o transforma em uma estrutura python e a retorna.
+        soup = BeautifulSoup(string, 'html.parser')
+        tabela = soup.find(name='table')
+
+        #lê o html e transforma novamente em string
+        #use .head(NUMERO_QUALQUER) para buscar somente os primeiros NUMERO_QUALQUER resultados
+        #pd.read_html(str(tabela))[0].head(10)
+        return pd.read_html(str(tabela))[0]
+
+    def transformarDictToJSON(self, dicionario, nome_json):
+
+        json_file = json.dumps(dicionario)
+        fp = open(nome_json, 'w')
+        fp.write(json_file)
+        fp.close
+
+    def removerArquivos(self, arquivos):
+
+        for arquivo in arquivos:
+            os.remove(arquivo)
